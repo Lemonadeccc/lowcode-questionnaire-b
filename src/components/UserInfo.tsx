68143394b04cbedbeeb482ac506996066ce4a4b5
Lemonadeccc <zwjhb12@163.com>
2024-02-29 19:35:44 +0800

    feat(redux): add redux for components

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -6,15 +6,22 @@ import { useRequest } from "ahooks"
 import { UserOutlined } from "@ant-design/icons"
 import { Button, message } from "antd"
 import { removeToken } from "../utils/user-token"
+import useGetUserInfo from "../hooks/useGetUserInfo"
+import { useDispatch } from "react-redux"
+import { logoutReducer } from "../store/userReducer"
 
 const UserInfo: FC = () => {
   const nav = useNavigate()
+  const dispatch = useDispatch()
   //对于已登录的用户显示什么，后续在做
 
-  const { data } = useRequest(getUserInfoService)
-  const { username, nickname } = data || {}
+  // const { data } = useRequest(getUserInfoService)
+  // const { username, nickname } = data || {}
+
+  const { username, nickname } = useGetUserInfo()
 
   function logout() {
+    dispatch(logoutReducer()) //清空了redux user数据
     removeToken() //清除token的存储
     message.success("退出成功")
     nav(LOGIN_PATHNAME)
