dea2aa27a282cee199ae146019e7dca39819c985
Lemonadeccc <zwjhb12@163.com>
2024-02-29 20:47:11 +0800

    feat(edit component): add Edit component

---
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react"
+import styles from "./EditCanvas.module.scss"
+
+//临时静态展示一下Title Input的效果
+
+import QuestionTitle from "../../../components/QuestionComponents/QuestionTitle/Component"
+import QuestionInput from "../../../components/QuestionComponents/QuestionInput/Component"
+
+const EditCanvas: FC = () => {
+  return (
+    <div className={styles.canvas}>
+      <div className={styles["component-wrapper"]}>
+        <div className={styles.component}>
+          <QuestionTitle />
+        </div>
+      </div>
+      <div className={styles["component-wrapper"]}>
+        <div className={styles.component}>
+          <QuestionInput />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EditCanvas
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react"
 // import { useParams } from "react-router-dom"
 // import { getQuestionService } from "../../../services/question"
 import useLoadQuestionData from "../../../hooks/useLoadQuestionData"
+import EditCanvas from "./EditCanvas"
+import styles from "./index.module.scss"
 
 const Edit: FC = () => {
   // const { id = "" } = useParams()
@@ -9,9 +11,23 @@ const Edit: FC = () => {
   const { loading, data } = useLoadQuestionData()
 
   return (
-    <div>
-      <p>Edit page</p>
-      {loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>}
+    <div className={styles.container}>
+      {/* <p>Edit page</p>
+      {loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>} */}
+      <div style={{ backgroundColor: "#fff", height: "40px" }}>Header</div>
+      <div className={styles["content-wrapper"]}>
+        <div className={styles.content}>
+          <div className={styles.left}>Left</div>
+          <div className={styles.main}>
+            <div className={styles["canvas-wrapper"]}>
+              {/* <div style={{height:'900px'}}>画布，测试滚动</div>
+               */}
+              <EditCanvas />
+            </div>
+          </div>
+          <div className={styles.right}>Right</div>
+        </div>
+      </div>
     </div>
   )
 }
